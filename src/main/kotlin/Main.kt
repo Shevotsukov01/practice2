f8529931c09e4ed3848716669c39c86d01b84296
user09 <harchenko.a0610>
2022-10-19 16:01:48 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+fun main(args: Array<String>) {
+    /*
+      //задание 1
+      val age1: Int = 42
+      val age2: Int = 21
+      //задание 2
+      val age1: Int = 42
+      val age2: Int = 21
+      val avg1: Int = (age1 + age2) / 2
+      //задание 3
+      val age1: Double = 42.0
+      val age2: Double = 21.0
+      val avg1: Double = (age1 + age2) / 2
+      print(avg1)
+      //задание 4
+      val firstName :String =  "Timur"
+      val lastName :String =  "Shevotsukov"
+      //задание 5
+      val firstName :String =  "Timur"
+      val lastName :String =  "Shevotsukov"
+      val fullName :String =  firstName + " " + lastName
+      println(fullName)
+      //задание 6
+      val firstName :String =  "Timur"
+      val lastName :String =  "Shevotsukov"
+      val fullName :String =  firstName + " " + lastName
+      val myDetails :String =  "Привет, меня зовут"
+      println(myDetails + " " + fullName)
+      //задание 7
+      val dates = Triple(13,10,2015)
+      //задание 8
+      val day = dates.first
+      val month = dates.second
+      val year = dates.third
+      /*println(day)
+      println(month)
+      println(year)*/
+      //задание 9
+      val monthANDyear = """
+              |$month
+              |$year
+      """.trimMargin()
+      println(monthANDyear)
+      */
+
+}
